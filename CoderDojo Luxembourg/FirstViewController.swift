0e5cc73bd7e26e8042202332248e3cbb4cda3e0a
Steve Clement <steve@localhost.lu>
2015-04-30 10:02:30 +0200

    Initial Commit

---
+++ CoderDojo Luxembourg/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CoderDojo Luxembourg
+//
+//  Created by Steve Clement on 30/04/15.
+//  Copyright (c) 2015 Steve Clement. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoderDojo Luxembourg/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CoderDojo Luxembourg
+//
+//  Created by Steve Clement on 30/04/15.
+//  Copyright (c) 2015 Steve Clement. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
