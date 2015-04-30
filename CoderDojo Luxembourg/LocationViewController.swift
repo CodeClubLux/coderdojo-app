43cae5c5e94c7ef0a3297e4759425425e147318a
Steve Clement <steve@localhost.lu>
2015-04-30 13:55:48 +0200

    - Initial commit

---
+++ CoderDojo Luxembourg/LocationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LocationViewController.swift
+//  CoderDojo Luxembourg
+//
+//  Created by Steve Clement on 30/04/15.
+//  Copyright (c) 2015 Steve Clement. All rights reserved.
+//
+
+import Foundation
